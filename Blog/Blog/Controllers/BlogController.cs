e227ab6b669ea6a5f4064e39018d5df546f07605
soloduhavlad <soloduhavlad@A24COMP6>
2018-11-29 17:44:56 +0200

    init commit

---
+++ Blog/Blog/Controllers/BlogController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
